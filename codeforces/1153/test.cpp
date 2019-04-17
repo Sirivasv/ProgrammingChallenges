25e7816efcd9884338a62442253371b23de9da01
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2019-04-17 18:14:37 -0500

    Test File

---
+++ codeforces/1153/test.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int T;
+    cin >> T;
+
+    cout << (T + 5) << '\n';
+
+    return 0;
+}
