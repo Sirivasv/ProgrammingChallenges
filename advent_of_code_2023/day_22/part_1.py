384d7608093be8760b4f4dfe95e3e74b2e01d1b8
Saul Ivan Rivas Vega <saul.ivan.rivas.vega@gmail.com>
2023-12-22 13:28:25 +0100

    Advent of code 2023 Day 22 part 1

--- advent_of_code_2023/day_22/part_1.py
+++ advent_of_code_2023/day_22/part_1.py
@@ -44,9 +44,9 @@ support_base = []
 for pos_i in range(global_min_i, global_max_i + 1):
   support_base.append([])
   for pos_j in range(global_min_j, global_max_j + 1):
-    support_base[pos_i].append(-1)
+    support_base[pos_i].append([-1, 0])
 
-will_support = {}
+will_support_alone = {}
 for brick_id in range(len(bricks)):
   brick = bricks[brick_id]
   curr_min_i = min(brick[0][2], brick[1][2])
@@ -57,16 +57,36 @@ for brick_id in range(len(bricks)):
 
   curr_min_z = min(brick[0][0], brick[1][0])
   curr_max_z = max(brick[0][0], brick[1][0])
+  curr_height = curr_max_z - curr_min_z + 1
+  
+  max_height_support_bases = {}
+  max_height = -1
+  for pos_i in range(curr_min_i, curr_max_i + 1):
+    for pos_j in range(curr_min_j, curr_max_j + 1):
+      support_base_height = support_base[pos_i][pos_j][1]
+      support_base_id = support_base[pos_i][pos_j][0]
+
+      if support_base_height > max_height:
+        max_height = support_base_height
+        max_height_support_bases = {support_base_id: max_height}
+      elif support_base_height == max_height:
+        max_height_support_bases[support_base_id] = max_height
+
+  # print(max_height_support_bases)
+  if (max_height > 0) and (len(max_height_support_bases) == 1):
+    for support_brick_id in max_height_support_bases:
+      will_support_alone[support_brick_id] = 1
   
   for pos_i in range(curr_min_i, curr_max_i + 1):
     for pos_j in range(curr_min_j, curr_max_j + 1):
-      if support_base[pos_i][pos_j] != -1:
-        will_support[support_base[pos_i][pos_j]] = 1
-      else:
-        support_base[pos_i][pos_j] = brick_id
+      curr_support_base_height = support_base[pos_i][pos_j][1]
+      curr_support_base_id = support_base[pos_i][pos_j][0]
+      support_base[pos_i][pos_j][0] = brick_id
+      support_base[pos_i][pos_j][1] = max_height + curr_height
 
+# print(will_support_alone)
 final_sum = len(bricks)
 for brick_id in range(len(bricks)):
-  if brick_id in will_support:
+  if brick_id in will_support_alone:
     final_sum -= 1
 print(final_sum)
--- advent_of_code_2023/day_22/part_2.py
+++ advent_of_code_2023/day_22/part_2.py
@@ -1,20 +1,92 @@
 import sys
-import math
 
-directions_i = [0, 1, 0, -1]
-directions_j = [1, 0, -1, 0]
+input_lines = [line.rstrip().split("~") for line in sys.stdin]
 
-input_map = [[*line.rstrip()] for line in sys.stdin]
+bricks = []
+for input_line in input_lines:
+  brick_end_1 = [int(x) for x in input_line[0].split(",")]
+  brick_end_2 = [int(x) for x in input_line[1].split(",")]
+  brick_end_1.reverse()
+  brick_end_2.reverse()
+  brick_i = [brick_end_1, brick_end_2]
+  bricks.append(brick_i)
 
-map_len_i = len(input_map)
-map_len_j = len(input_map[0])
+bricks.sort()
 
-max_steps = 481843
-for curr_count in range(2, max_steps + 1):
-  curr_gcd = math.gcd(curr_count, max_steps)
-  print(curr_count)
-  print(curr_gcd)
-  if curr_gcd == curr_count:
-    print("Found")
-    break
-  print("-------------")
+global_min_i = bricks[0][0][2]
+global_min_j = bricks[0][0][1]
+global_min_z = bricks[0][0][0]
+
+global_max_i = bricks[0][0][2]
+global_max_j = bricks[0][0][1]
+global_max_z = bricks[0][0][0]
+
+for brick in bricks:
+  global_min_i = min(global_min_i, brick[0][2])
+  global_min_i = min(global_min_i, brick[1][2])
+
+  global_min_j = min(global_min_j, brick[0][1])
+  global_min_j = min(global_min_j, brick[1][1])
+
+  global_min_z = min(global_min_z, brick[0][0])
+  global_min_z = min(global_min_z, brick[1][0])
+
+  global_max_i = max(global_max_i, brick[0][2])
+  global_max_i = max(global_max_i, brick[1][2])
+
+  global_max_j = max(global_max_j, brick[0][1])
+  global_max_j = max(global_max_j, brick[1][1])
+
+  global_max_z = max(global_max_z, brick[0][0])
+  global_max_z = max(global_max_z, brick[1][0])
+
+support_base = []
+for pos_i in range(global_min_i, global_max_i + 1):
+  support_base.append([])
+  for pos_j in range(global_min_j, global_max_j + 1):
+    support_base[pos_i].append([-1, 0])
+
+will_support_alone = {}
+for brick_id in range(len(bricks)):
+  brick = bricks[brick_id]
+  curr_min_i = min(brick[0][2], brick[1][2])
+  curr_max_i = max(brick[0][2], brick[1][2])
+
+  curr_min_j = min(brick[0][1], brick[1][1])
+  curr_max_j = max(brick[0][1], brick[1][1])
+
+  curr_min_z = min(brick[0][0], brick[1][0])
+  curr_max_z = max(brick[0][0], brick[1][0])
+  curr_height = curr_max_z - curr_min_z + 1
+  
+  max_height_support_bases = {}
+  max_height = -1
+  for pos_i in range(curr_min_i, curr_max_i + 1):
+    for pos_j in range(curr_min_j, curr_max_j + 1):
+      support_base_height = support_base[pos_i][pos_j][1]
+      support_base_id = support_base[pos_i][pos_j][0]
+
+      if support_base_height > max_height:
+        max_height = support_base_height
+        max_height_support_bases = {support_base_id: max_height}
+      elif support_base_height == max_height:
+        max_height_support_bases[support_base_id] = max_height
+
+  # print(max_height_support_bases)
+  if (max_height > 0) and (len(max_height_support_bases) == 1):
+    for support_brick_id in max_height_support_bases:
+      will_support_alone[support_brick_id] = 1
+  
+  for pos_i in range(curr_min_i, curr_max_i + 1):
+    for pos_j in range(curr_min_j, curr_max_j + 1):
+      curr_support_base_height = support_base[pos_i][pos_j][1]
+      curr_support_base_id = support_base[pos_i][pos_j][0]
+      support_base[pos_i][pos_j][0] = brick_id
+      support_base[pos_i][pos_j][1] = max_height + curr_height
+
+print(will_support_alone)
+final_sum = len(bricks)
+for brick_id in range(len(bricks)):
+  if brick_id in will_support_alone:
+    final_sum -= 1
+print(final_sum)
